550af3ca16813b2cf7f16f91cb13931ced8b70fc
bauyrzhan <newnewernewest123@gmail.com>
2024-12-03 09:56:04 +0500

    model

--- authapp/settings.py
+++ authapp/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'authapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'authapp',
+        'USER': 'nurbergen',
+        'PASSWORD': 'qwerty123',
     }
 }
 
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.contrib.auth.models import AbstractUser
+
+class user(AbstractUser):
+    name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    address = models.CharField(max_length=100)
+    age = models.IntegerField()
+    password = models.CharField(max_length=100)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+
+
+
+
+
+
+
+
